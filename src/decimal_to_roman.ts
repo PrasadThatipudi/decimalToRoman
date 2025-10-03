ef0b8d5e3e6c35898d0cc325a623349450fba33d
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-10-03 17:11:11 +0530

    Logic works for first 10 numbers

--- src/decimal_to_roman.ts
+++ src/decimal_to_roman.ts
@@ -1,27 +1,32 @@
 const nearestStandardNumber = (number: number): [number, string] | null => {
-  const standardNumbers: [number, string][] = [
-    [5, "V"],
-    [1, "I"],
+  const standardNumbers: [number, number, string][] = [
+    [1, 3, "I"],
+    [5, 8, "V"],
+    [10, 30, "X"],
   ];
 
-  for (const [standardNumber, symbol] of standardNumbers) {
-    if (number >= standardNumber) return [standardNumber, symbol];
+  for (const [standardNumber, limit, symbol] of standardNumbers) {
+    if (number <= limit) return [standardNumber, symbol];
   }
 
   return null;
 };
 
-const decimalToRoman = (decimalNumber: number, acc = ""): string => {
+const decimalToRoman = (decimalNumber: number): string => {
   const standardNumberSet = nearestStandardNumber(decimalNumber);
 
   if (!standardNumberSet) throw "Invalid Number";
 
   const [standardNumber, symbol] = standardNumberSet;
+
+  if (decimalNumber < standardNumber)
+    return decimalToRoman(standardNumber - decimalNumber) + symbol;
+
+  if (decimalNumber === standardNumber) return symbol;
+
   const remainingNumber = decimalNumber - standardNumber;
 
-  if (remainingNumber <= 0) return acc + symbol;
-
-  return decimalToRoman(remainingNumber, acc + symbol);
+  return symbol + decimalToRoman(remainingNumber);
 };
 
 export { decimalToRoman };
